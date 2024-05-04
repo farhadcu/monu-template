1daa9fb56950256e688c60e5e2e197ce89225413
farhadcu <fmfarhad231@gmail.com>
2024-05-04 19:35:11 +0600

    slider and features section- class 15

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    // slick 
+    $('.slider-active').slick({
+        infinite: true,
+        slidesToShow: 1,
+        arrows: false,
+        dots:false,
+        autoplay:true,
+        slidesToScroll: 1,
+      });
+
+
+
+
+})
