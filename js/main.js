85b41507a78011b3f9062476d4b0d82741b7d638
farhadcu <fmfarhad231@gmail.com>
2024-05-20 02:59:06 +0600

    responsive design and also updated into meanmenu.css file - class 19

--- js/main.js
+++ js/main.js
@@ -1,45 +1,71 @@
-$(document).ready(function(){
-    // slick for slider-active
-    $('.slider-active').slick({
-        infinite: true,
-        slidesToShow: 1,
-        arrows: false,
-        dots:false,
-        autoplay:true,
-        slidesToScroll: 1,
-      });
+$(document).ready(function () {
 
-    // slick for testimonial-active
-    $('.testimonial-active').slick({
-        infinite: true,
-        slidesToShow: 1,
-        arrows: false,
-        dots:true,
-        autoplay:true,
-        slidesToScroll: 1,
-      });
+  // meanMenu
+  $('#mobile-menu-active').meanmenu({
+    meanScreenWidth: "991",
+    meanMenuContainer: '.mobile-menu'
+  });
 
-    // slick for brand-active
-    $('.brand-active').slick({
-        infinite: true,
-        slidesToShow: 5,
-        arrows: false,
-        dots:false,
-        autoplay:true,
-        slidesToScroll: 1,
-      });
+  // slick for slider-active
+  $('.slider-active').slick({
+    infinite: true,
+    slidesToShow: 1,
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    slidesToScroll: 1,
+  });
 
-      //counter js
-      $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
+  // slick for testimonial-active
+  $('.testimonial-active').slick({
+    infinite: true,
+    slidesToShow: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    slidesToScroll: 1,
+  });
 
-  
+  // slick for brand-active
+  $('.brand-active').slick({
+    infinite: true,
+    slidesToShow: 5,
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
-
-
-
-
+  //counter js
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+  });
 
 })
